368f9875bd25f2a5ed123eb5749bb40aa4d30cbd
Mariano Troncoso <petotronco@gmail.com>
2020-10-09 15:33:08 -0300

    first command parse

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
 
-require('dotenv').config();
+const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
     // console.log(`Logged in as ${client.user.tag}!`);
-    console.log('Secretario a tu servicio');
+    console.log('Secretaria a tu servicio');
 });
 
+// accion y materia 
+// ej: apunte recursos 
+// ej : clase (link)recursos 
+
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('Pong!');
+
+    // no tiene prefijo del comando o el autor del mensaje es el bot
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    // quito el prefijo
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    console.log(args)
+    const command = args.map(arg => arg.toLocaleLowerCase());
+    // command[0] = accion, command[1] = materia 
+
+    if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong.');
+    } else if (message.content === `${prefix}beep`) {
+        message.channel.send('Boop.');
     }
 
-    if (message.content === 'ejemplo') {
+    if (message.content === `${prefix}ejemplo`) {
         const embed = new MessageEmbed()
             .setTitle('APUNTE: Recursos')
             .setColor('ORANGE')
-            // .setURL('https://docs.google.com/document/d/1n5zrD9G2BMEi-B-B6unI4cZGUC74a9IoIGAd_WYbLb4/edit')
             .setDescription('https://docs.google.com/document/d/1n5zrD9G2BMEi-B-B6unI4cZGUC74a9IoIGAd_WYbLb4/edit');
 
         message.channel.send(embed);
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
