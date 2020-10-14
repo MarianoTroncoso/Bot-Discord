8a1a6e35996a17b4188dde3bc6f7f7431866e229
Mariano Troncoso <petotronco@gmail.com>
2020-10-14 15:22:38 -0300

    list apunte and list clase

--- commands/listApunte.js
+++ commands/listApunte.js
@@ -1,14 +1,22 @@
 const { MessageEmbed } = require('discord.js');
+const Apunte = require('../models/apunte');
 
 module.exports = {
     name: 'listApunte',
 	description: 'list all the apuntes',
-    execute (message,  args) {
+    async execute (message,  args) {
+
+        // arreglo de materias
+        var materias = (await Apunte.find({}).select('materia -_id')).map( x => x.materia)
+
+        if(!materias.length > 0){
+            materias = 'ninguna'
+        }
 
         const embed = new MessageEmbed()
-            .setTitle('NO IMPLEMENTADO AUN')
-            .setColor('RED')
-            // .setDescription('OBTENER EL ENLACE A UN APUNTE:\n!get apunte [materia]\n(para conocer las materias cuyos apuntes están disponibles utilizar: !list apunte)\n\nOBTENER EL ENLACE A UNA CLASE VIRTUAL:\n!get clase [materia]\n(para conocer las materias cuyas clases están disponibles utilizar: !list clase)\n\nPARA SETEAR ALGO habla con petito :D' );
+            .setTitle('Hay APUNTES disponibles para las materias:')
+            .setColor('PURPLE')
+            .setDescription(materias);
 
         message.channel.send(embed);
     }
